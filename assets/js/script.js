7dbb5eab3d37f90d82a2109df3398b4d73a357e2
lele2dot0 <22071615+lele2dot0@users.noreply.github.com>
2022-05-11 13:10:54 +0100

    Store completed UI for Work Psy
    - upload old completed project

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const moreToggle = () => {
+  const toggleButton = document.querySelector('.carolann');
+        //Toggle Nav
+        toggleButton.classList.toggle("d-none");
+}
+
+const naomiToggle = () => {
+  const toggleButton = document.querySelector('.naomi');
+        //Toggle Nav
+        toggleButton.classList.toggle("d-none");
+}
+
+
+
